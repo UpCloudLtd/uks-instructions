267c28087fcde08a00f39be0cedb8c080afda31b
ka-myl <kamillcybulski+2@gmail.com>
2022-10-11 17:02:25 +0200

    docs: update docs for terraform setup (#4)

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,5 +1,8 @@
 # Gather available pricing plans for Kubernetes node groups via data source
-data "upcloud_kubernetes_plans" "example" {}
+data "upcloud_kubernetes_plan" "small" {
+  # Name can be "small", "medium" or "large"
+  name = "small"
+}
 
 # Gather details of the newly created cluster via data source
 data "upcloud_kubernetes_cluster" "example" {
